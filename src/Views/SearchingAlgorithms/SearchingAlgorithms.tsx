9ffedc4a5c4bd2afea065b11acd827d6d7e2f68d
Khem Singh Dhami <sagardhami2001@gmail.com>
2023-10-29 21:53:51 +0545

    generated array according to length

--- src/Views/SearchingAlgorithms/SearchingAlgorithms.tsx
+++ src/Views/SearchingAlgorithms/SearchingAlgorithms.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function SearchingAlgorithms() {
-  return <div>SearchingAlgorithms</div>;
+  const [length, setLength] = useState<number>(0);
+  const [array, setArray] = useState<number[]>([]);
+
+  const handleChange = (e: any) => {
+    setLength(e.target.value);
+  };
+
+  useEffect(() => {
+    function generateRandomArray(length: number): number[] {
+      const randomArray: number[] = [];
+      for (let i = 0; i < length; i++) {
+        let randomElement = Math.floor(Math.random() * 100);
+        randomArray.push(randomElement);
+      }
+      return randomArray;
+    }
+    setArray(generateRandomArray(length));
+  }, [length]);
+  console.log(array, "This is array");
+  return (
+    <div>
+      <div>
+        <label>Enter the length of Array:</label>
+        <input type="number" onChange={handleChange} max="20" min="5" />
+
+        <div className="array-container">
+          {array &&
+            array.map((item, index) => (
+              <span key={index} className="a-box">
+                {item}
+              </span>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchingAlgorithms;
