f214ac1c52d916648d3d9b65623f45a7fa71496d
Khem Singh Dhami <sagardhami2001@gmail.com>
2023-10-29 10:39:06 +0545

    feat: added animation

--- src/Views/SortingAlgorithms/SortingAlgorithms.tsx
+++ src/Views/SortingAlgorithms/SortingAlgorithms.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Sorting = () => {
   const array = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
-    <div className="flex flex-center">
+    <div className="flex flex-center flex-align-center">
       <div className="array-container">
         {array.map((item, index) => (
           <span key={index} className="a-box">
