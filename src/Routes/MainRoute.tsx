8085ba64761166c3704909b79c123d842f8fa5fa
Khem Singh Dhami <sagardhami2001@gmail.com>
2023-10-30 17:33:20 +0545

    fix: main layout fix

--- src/Routes/MainRoute.tsx
+++ src/Routes/MainRoute.tsx
@@ -8,12 +8,12 @@ import MainLayout from "../Layouts/MainLayout";
 export const MainRoute = createBrowserRouter([
   {
     path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/",
     element: <MainLayout />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
       {
         path: "/pathfind",
         element: <Pathfind />,
