7c7553abeecb523997e39e058682e004a581b841
razesh10 <razesh.leo19@gmail.com>
2023-11-01 20:20:12 +0545

    homepage edit

--- src/Views/Homepage/Homepage.tsx
+++ src/Views/Homepage/Homepage.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import "./Homepage.css";
+import Navbar from "../../Components/Navbar/Navbar";
 
 export const Home = () => {
   return (
     <>
-      <div className="hero-text">
-        <p>
-          Get better understanding of <br />
-          <span>ALGORITHMS</span> through <br />
-          <span>VISUALIZATION.</span>
-        </p>
-      </div>
-      <div className="btns">
-        <div className="startbtn">Start Visualizing</div>
-        <div className="comparebtn">Compare</div>
-      </div>
+      <body>
+        <Navbar />
+        <div className="hero-text">
+          <p>
+            Get better understanding of <br />
+            <span>ALGORITHMS</span> through <br />
+            <span>VISUALIZATION.</span>
+          </p>
+        </div>
+        <div className="btns">
+          <div className="startbtn">Start Visualizing</div>
+          <div className="comparebtn">Compare</div>
+        </div>
+      </body>
     </>
   );
 };
