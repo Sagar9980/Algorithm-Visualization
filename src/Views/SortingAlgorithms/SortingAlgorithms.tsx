3ee227f012582a04d7511527e323d2a8cf6cc9b5
Khem Singh Dhami <sagardhami2001@gmail.com>
2023-12-09 20:50:34 +0545

    bubble sort implementation

--- src/Views/SortingAlgorithms/SortingAlgorithms.tsx
+++ src/Views/SortingAlgorithms/SortingAlgorithms.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { bubbleSort } from "../../Algorithms/SortingAlgorithms/bubbleSort";
 
 export const Sorting = () => {
   const [length, setLength] = useState<number>(0);
   const [algorithm, setAlgorithm] = useState<string>("0");
-  const [array, setArray] = useState<number[]>([]);
+  const [array, setArray] = useState<number[]>([40, 74, 4]);
 
   const handleChange = (e: any) => {
     setLength(e.target.value);
@@ -13,7 +14,11 @@ export const Sorting = () => {
   };
 
   const triggerAlgorithm = () => {
-    console.log("algo");
+    switch (algorithm) {
+      case "0":
+        // bubbleSort(array);
+        console.log(bubbleSort(array));
+    }
   };
   useEffect(() => {
     function generateRandomArray(length: number): number[] {
